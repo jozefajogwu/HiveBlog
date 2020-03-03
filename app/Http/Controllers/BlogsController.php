8dad9a2eae89b64f44a217dcbfc840bf9edc0fae
Your <Your.name@youremail.com>
2020-03-03 10:21:26 +0100

    first commit to github

---
+++ app/Http/Controllers/BlogsController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Blog;
+
+use Illuminate\Http\Request;
+
+class BlogsController extends Controller
+{
+    public function index(){
+
+        $blogs = Blog:: all();
+        return view('blogs.index' , compact('blogs'));
+    }
+
+    public function create(){
+        return view('blogs.create');
+    }
+
+
+    public function store(Request $request){
+
+
+        $input = $request->all();
+        $blog = Blog::create($input);
+
+       // $blog = new Blog();
+      //  $blog->title = $request->title;
+      //  $blog->body = $request->body;
+      //  $blog->save();
+        return redirect('/blogs');
+        
+    }
+
+    public function show($id){
+        $blog = Blog::findOrFail($id);
+        return view('blogs.show', compact('blog'));
+    }
+
+
+    public function edit($id){
+        $blog = Blog::findOrFail($id);
+        return view('blogs.edit',['blog' => $blog]);
+
+    }
+
+
+    public function update(Request $request, $id){
+        $input = $request->all();
+        $blog = Blog::findOrFail($id);
+        $blog->update($input);
+        return redirect('blogs');
+        
+
+    }
+
+    public function delete(Request $request, $id){
+        $blog = Blog::findOrFail($id);
+        $blog->delete();
+        return redirect('blogs');
+    }
+
+
+    public function trash(){
+        $trashedBlogs = Blog::onlyTrashed()->get();
+        return view('blogs.trash', compact('trashedBlogs'));
+
+    }
+
+    public function restore($id){
+        $restoredBlog = Blog::onlyTrashed()->findOrFail($id);
+        $restoredBlog->restore($restoredBlog);
+        return redirect('blogs');
+        
+    }
+
+    public function permanentDelete($id){
+        $permanentDeleteBlog = Blog::onlyTrashed()->FindOrFail($id);
+        $permanentDeleteBlog->forceDelete($permanentDeleteBlog);
+        return back();
+    }
+}
